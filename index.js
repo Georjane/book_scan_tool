7ddc03044654f45f0d5f925ebf01f12143b4fd63
Georjane <pzgeorjane@gmail.com>
2021-03-02 10:51:00 +0100

    Install multer to upload pdf single  book

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const path = require("path") 
-
+const multer = require('multer')
 
 const app = express();
 
-app.use(express.static(__dirname+'/assets/images'));
 app.set("views",path.join(__dirname,"views")) 
 app.set("view engine","ejs")
+
+var upload = multer({ 
+	// storage: storage, 
+	fileFilter: function (req, file, cb){
+		var filetypes = /pdf/; 
+		var mimetype = filetypes.test(file.mimetype);
+		var extname = filetypes.test(path.extname(file.originalname).toLowerCase()); 		
+		if (mimetype && extname) { 
+			return cb(null, true); 
+		} 	
+		cb("Error: File upload only supports the "
+				+ "following filetypes - " + filetypes); 
+	}
+}).single("mybook");
+
+app.post("/processpdfbook", async (req, res, next) => {
+	upload(req,res,function(err) {
+		if(err) { 
+			res.next(err);
+		} else {
+			// return scan id in progress while waiting for processPDF to complete
+			// processPDF(res);
+		}
+	});
+}) 
 
 app.get("/",function(req,res){ 
   res.render("homepage"); 
