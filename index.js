795b966ce441d7cb1bc45fa2128027dff3f70f6e
Georjane <pzgeorjane@gmail.com>
2021-03-02 15:53:45 +0100

    Install multer and pdf parser to upload book and process

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const path = require("path") 
 const multer = require('multer')
+const fs = require('fs');
+const pdf = require('pdf-parse');
 
 const app = express();
 
 app.set("views",path.join(__dirname,"views")) 
 app.set("view engine","ejs")
 
+var storage = multer.diskStorage({ 
+	destination: function (req, file, cb) {  
+		cb(null, "uploads") 
+	}, 
+	filename: function (req, file, cb) { 
+		cb(null, file.fieldname +".pdf") 
+	} 
+})  
+
 var upload = multer({ 
-	// storage: storage, 
+	storage: storage, 
 	fileFilter: function (req, file, cb){
 		var filetypes = /pdf/; 
 		var mimetype = filetypes.test(file.mimetype);
@@ -24,13 +35,40 @@ var upload = multer({
 app.post("/processpdfbook", async (req, res, next) => {
 	upload(req,res,function(err) {
 		if(err) { 
-			res.next(err);
+			res.send(err);
 		} else {
 			// return scan id in progress while waiting for processPDF to complete
-			// processPDF(res);
+			processPDF(res);
 		}
 	});
 }) 
+
+const processPDF = async (res) => {
+	let words = await convertPdfToTxt('uploads/mybook.pdf');
+	let wordsToCount = await groupWordsBycount(words);
+	const wordsArray = Object.keys(wordsToCount)
+  console.log(wordsToCount);
+	// scanBook(wordsArray, res)
+}
+
+const convertPdfToTxt = async (uploadedBookPath) => {
+	let dataBuffer = fs.readFileSync(uploadedBookPath); 
+	let words = [];
+	await pdf(dataBuffer).then( (data) => {
+		words = data.text.replace(/[^a-zA-Z ]/g, "").split(' ');
+	});
+	return words;
+};
+
+const groupWordsBycount = async (words) => {
+	let wordsToCount = {};
+	for(let i = 0; i < words.length; i=i+1) {
+		if(wordsToCount[(words[i]).toLowerCase()] == undefined) 
+			wordsToCount[(words[i]).toLowerCase()] = 0; 	
+		wordsToCount[(words[i]).toLowerCase()]++;
+	}
+	return wordsToCount;
+};
 
 app.get("/",function(req,res){ 
   res.render("homepage"); 
