d8d04f378e68519e9c46ba1c4fa2810f359388cd
Georjane <pzgeorjane@gmail.com>
2021-03-09 15:45:46 +0100

    Display processpdf page even with large book upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const fs = require('fs');
 const pdf = require('pdf-parse');
 const csv = require('csv-parser');
+const bodyParser = require('body-parser');
 
 
 const app = express();
@@ -14,9 +15,13 @@ const wordsNotFound = [];
 const arrayOfWordIds = [];
 const wordAndId = {};
 let percentageMatch = 0;
+let scanid = 1;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public')); 
+app.set("views",path.join(__dirname,"views")) 
+app.set("view engine","ejs") 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
@@ -37,31 +42,9 @@ const upload = multer({
       return cb(null, true);
     }
     cb(`${'Error: File upload only supports the '
-				+ 'following filetypes - '}${filetypes}`);
+      + 'following filetypes - '}${filetypes}`);
   },
 }).single('mybook');
-
-app.post('/processpdfbook', async (req, res, next) => {
-  upload(req, res, (err) => {
-    if (err) {
-      res.send(err);
-    } else {
-      // return scan id in progress while waiting for processPDF to complete
-      processPDF(res);
-    }
-  });
-});
-
-const processPDF = async (res) => {
-  const words = await convertPdfToTxt('uploads/mybook.pdf');
-  const wordsToCount = await groupWordsBycount(words);
-  const wordsArray = Object.keys(wordsToCount);
-  for (const word in wordsToCount) {
-    allBookWordsObject[word] = {};
-    allBookWordsObject[word].word_count = wordsToCount[word];
-  }
-  scanBook(wordsArray, res);
-};
 
 const scanBook = (words, res) => {
   fs.createReadStream('words.csv')
@@ -82,7 +65,7 @@ const scanBook = (words, res) => {
       const unfoundWordsWithPartlyMatchedWords = partlyMatchedWords(wordsNotFound, dictionary);
       for (const word in allBookWordsObject) {
         for (const unfoundWord in unfoundWordsWithPartlyMatchedWords) {
-          if (word == unfoundWord) {
+          if (word === unfoundWord) {
             allUnfoundWordsObject[word] = {};
             allUnfoundWordsObject[word].word_count = allBookWordsObject[word].word_count;
             // allUnfoundWordsObject[word]['match-words'] = unfoundWordsWithPartlyMatchedWords[word]
@@ -116,11 +99,44 @@ const convertPdfToTxt = async (uploadedBookPath) => {
   return words;
 };
 
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+}
+
+const findMaxPercentageMatch = (object) => {
+  const percentValuesArray = Object.values(object);
+  return Math.max(...percentValuesArray);
+};
+
+const findMeaningAndDescription = async (res, req) => {
+  fs.createReadStream('roots.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      for (const wordNotFound in allUnfoundWordsObject) {
+        if (row.root_id === allUnfoundWordsObject[wordNotFound].root_id) {
+          allUnfoundWordsObject[wordNotFound].description = row.description;
+          allUnfoundWordsObject[wordNotFound].meaning = row.Meaning;
+        }
+      }
+    })
+    .on('end', async () => {
+      for (const unfoundword in allUnfoundWordsObject) {
+        delete allUnfoundWordsObject[unfoundword].word_id;
+        delete allUnfoundWordsObject[unfoundword].root_id;
+      }
+      // res.render('resultspage', { data: allUnfoundWordsObject, percentageMatch });
+    });
+};
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+}
+
 const groupWordsBycount = async (words) => {
   const wordsToCount = {};
   for (let i = 0; i < words.length; i += 1) {
-    if (wordsToCount[(words[i]).toLowerCase()] == undefined) { wordsToCount[(words[i]).toLowerCase()] = 0; }
-    wordsToCount[(words[i]).toLowerCase()]++;
+    if (wordsToCount[(words[i]).toLowerCase()] === undefined) { wordsToCount[(words[i]).toLowerCase()] = 0; }
+    wordsToCount[(words[i]).toLowerCase()] += 1;
   }
   return wordsToCount;
 };
@@ -134,41 +150,18 @@ const partlyMatchedWords = (words, dictionary) => {
       if (regexChildWord.test(parentWord)) {
         subhash[childWord] = wordPercentageMatch(childWord, parentWord);
       }
+      subhash
     });
     unfoundWordsWithPartlyMatchedWords[parentWord] = subhash;
   });
   return unfoundWordsWithPartlyMatchedWords;
 };
 
-const wordPercentageMatch = (childWord, parentWord) => (Math.round((childWord.length / parentWord.length) * 100));
-
-function escapeRegExp(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-}
-
-const findMaxPercentageMatch = (object) => {
-  const percentValuesArray = Object.values(object);
-  return Math.max(...percentValuesArray);
-};
-
-const wordsWithHighestPercentMatch = async (unfoundWordsWithPartlyMatchedWords, res) => {
-  for (const unfoundWord in unfoundWordsWithPartlyMatchedWords) {
-    const maxPercentMatchValue = findMaxPercentageMatch(unfoundWordsWithPartlyMatchedWords[unfoundWord]);
-    const maxPercentMatchWord = getKeyByValue(unfoundWordsWithPartlyMatchedWords[unfoundWord], maxPercentMatchValue);
-    for (const wordNotFound in allUnfoundWordsObject) {
-      if (unfoundWord == wordNotFound) {
-        allUnfoundWordsObject[wordNotFound].highest_match_word = maxPercentMatchWord;
-        allUnfoundWordsObject[wordNotFound].percent_match = `${maxPercentMatchValue}%`;
-        allUnfoundWordsObject[wordNotFound].word_id = findWordId(maxPercentMatchWord);
-      }
-    }
-  }
-  findRootId(res);
-};
+const wordPercentageMatch = (word, parent) => (Math.round((word.length / parent.length) * 100));
 
 const findWordId = (maxPercentMatchWord) => {
   for (const word in wordAndId) {
-    if (maxPercentMatchWord == wordAndId[word].word) {
+    if (maxPercentMatchWord === wordAndId[word].word) {
       return wordAndId[word].word_id;
     }
   }
@@ -200,7 +193,7 @@ const findRootId = (res) => {
       });
       for (const word_id in filteredUnfoundWordAndRootIds) {
         for (const wordNotFound in allUnfoundWordsObject) {
-          if (word_id == allUnfoundWordsObject[wordNotFound].word_id) {
+          if (word_id === allUnfoundWordsObject[wordNotFound].word_id) {
             allUnfoundWordsObject[wordNotFound].root_id = filteredUnfoundWordAndRootIds[word_id];
           }
         }
@@ -209,32 +202,69 @@ const findRootId = (res) => {
     });
 };
 
-const findMeaningAndDescription = async (res, req) => {
-  fs.createReadStream('roots.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-      for (const wordNotFound in allUnfoundWordsObject) {
-        if (row.root_id == allUnfoundWordsObject[wordNotFound].root_id) {
-          allUnfoundWordsObject[wordNotFound].description = row.description;
-          allUnfoundWordsObject[wordNotFound].meaning = row.Meaning;
-        }
+const wordsWithHighestPercentMatch = async (unfoundWordsWithPartlyMatchedWords, res) => {
+  for (const unfoundWord in unfoundWordsWithPartlyMatchedWords) {
+    const maxPercentMatchValue = findMaxPercentageMatch(unfoundWordsWithPartlyMatchedWords[unfoundWord]);
+    const maxPercentMatchWord = getKeyByValue(unfoundWordsWithPartlyMatchedWords[unfoundWord], maxPercentMatchValue);
+    for (const wordNotFound in allUnfoundWordsObject) {
+      if (unfoundWord === wordNotFound) {
+        allUnfoundWordsObject[wordNotFound].highest_match_word = maxPercentMatchWord;
+        allUnfoundWordsObject[wordNotFound].percent_match = `${maxPercentMatchValue}%`;
+        allUnfoundWordsObject[wordNotFound].word_id = findWordId(maxPercentMatchWord);
       }
-    })
-    .on('end', async () => {
-      for (const unfoundword in allUnfoundWordsObject) {
-        delete allUnfoundWordsObject[unfoundword].word_id;
-        delete allUnfoundWordsObject[unfoundword].root_id;
-      }
-      res.render('resultspage', { data: allUnfoundWordsObject, percentageMatch });
-    });
+    }
+  }
+  findRootId(res);
 };
 
-function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
-}
+const processPDF = async (res) => {
+  const words = await convertPdfToTxt('uploads/mybook.pdf');
+  const wordsToCount = await groupWordsBycount(words);
+  const wordsArray = Object.keys(wordsToCount);
+  for (const word in wordsToCount) {
+    allBookWordsObject[word] = {};
+    allBookWordsObject[word].word_count = wordsToCount[word];
+  }
+  scanBook(wordsArray, res);
+};
+
+app.post('/processpdfbook', async (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      // res.send(err);
+      scanid = -1
+      res.render('scanpage', {scanid: scanid});
+    } else {
+      // scanid = Math.floor(Math.random() * 100);
+      scanid = 1
+      res.render('scanpage', {scanid: scanid});
+      processPDF(res)
+    }
+  });
+});
 
 app.get('/', (req, res) => {
   res.render('homepage');
+});
+
+app.get('/scans/' + scanid, (req, res) => {
+  const scanData = JSON.stringify(allUnfoundWordsObject);
+  fs.writeFile('scans/scanid.json', scanData, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+  });
+
+  fs.readFile('scans/scanid.json', 'utf-8', (err, data) => {
+    if (err) {
+        throw err;
+    }  
+    // parse JSON object
+    const datascan = JSON.parse(data.toString());
+    res.render('resultspage', { data: datascan, percentageMatch });
+
+  });
 });
 
 app.listen(8080, (error) => {
