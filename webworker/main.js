160ce6c2ce2cbd9e074a84b1c9f15625f4d3ee23
Georjane <pzgeorjane@gmail.com>
2021-03-11 08:05:12 +0100

    Old code

--- webworker/main.js
+++
@@ -1,6 +0,0 @@
-// import Worker from 'web-worker';
-const Worker = require('web-worker');
-
-
-const worker = new Worker('data:,postMessage("hello")');
-worker.onmessage = e => console.log(e.data);  // "hello"
